f2d15d99e32a07f29dd1556461dc14e1c22b6d1a
Poudja CANESSANE <poudja.canessane@adevinta.com>
2022-02-17 17:14:12 +0100

    Create LBCUpdatableProtocol

---
+++ AdmaxDemo/Service/LBCUpdatableProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  LBCUpdatableProtocol.swift
+//  AdmaxDemo
+//
+//  Created by Poudja.canessane on 17/02/2022.
+//  Copyright © 2022 Admax. All rights reserved.
+//
+
+import AdmaxPrebidMobile
+protocol LBCUpdatableProtocol: UpdatableProtocol {}
