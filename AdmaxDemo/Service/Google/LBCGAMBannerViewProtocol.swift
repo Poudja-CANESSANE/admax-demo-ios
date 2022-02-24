28e2c802a20ad02193264d00fadd650d9456adbd
Poudja CANESSANE <poudja.canessane@adevinta.com>
2022-02-24 15:17:40 +0100

    Create LBCGAMBannerViewProtocol

---
+++ AdmaxDemo/Service/Google/LBCGAMBannerViewProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  LBCGAMBannerViewProtocol.swift
+//  AdmaxDemo
+//
+//  Created by Poudja.canessane on 24/02/2022.
+//  Copyright © 2022 Admax. All rights reserved.
+//
+
+import GoogleMobileAds
+
+protocol LBCGAMBannerViewProtocol: UIView {
+    var adUnitID: String? { get set }
+    var rootViewController: UIViewController? { get set }
+    var delegate: GADBannerViewDelegate? { get set }
+    var appEventDelegate: GADAppEventDelegate? { get set }
+
+    func load(_ request: GADRequest?)
+}
--- AdmaxDemo/Service/Google/LBCGAMBannerViewService.swift
+++ AdmaxDemo/Service/Google/LBCGAMBannerViewService.swift
@@ -8,15 +8,6 @@
 
 import GoogleMobileAds
 
-protocol LBCGAMBannerViewProtocol: UIView {
-    var adUnitID: String? { get set }
-    var rootViewController: UIViewController? { get set }
-    var delegate: GADBannerViewDelegate? { get set }
-    var appEventDelegate: GADAppEventDelegate? { get set }
-
-    func load(_ request: GADRequest?)
-}
-
 protocol LBCGAMBannerViewServiceProtocol: AnyObject {
     func createBannerView(adUnitId: String,
                           rootViewController: UIViewController?,
