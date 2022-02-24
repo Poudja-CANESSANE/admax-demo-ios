0966a471f4d407245339ac56ac45e2bd5b2a7d0d
Poudja CANESSANE <poudja.canessane@adevinta.com>
2022-02-24 14:35:49 +0100

    Create LBCGAMRequestProtocol2

---
+++ AdmaxDemo/Service/Google/LBCGAMRequestProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  LBCGAMRequestProtocol.swift
+//  AdmaxDemo
+//
+//  Created by Poudja.canessane on 24/02/2022.
+//  Copyright © 2022 Admax. All rights reserved.
+//
+
+import GoogleMobileAds
+
+protocol LBCGAMRequestProtocol2: GAMRequest {}
+
+extension GAMRequest: LBCGAMRequestProtocol2 {}
