0513ca7be7136a1c001ed0a736c467c19eb3c29d
Poudja CANESSANE <poudja.canessane@adevinta.com>
2022-02-24 16:54:30 +0100

    Create LBCGAMInterstitialAdProtocol

---
+++ AdmaxDemo/Service/Google/LBCGAMInterstitialAdProtocol.swift
@@ -0,0 +1,24 @@
+//
+//  LBCGAMInterstitialAdProtocol.swift
+//  AdmaxDemo
+//
+//  Created by Poudja.canessane on 24/02/2022.
+//  Copyright © 2022 Admax. All rights reserved.
+//
+
+import GoogleMobileAds
+
+protocol LBCGAMInterstitialAdProtocol2: AnyObject {
+    var appEventDelegate: GADAppEventDelegate? { get set }
+
+    func load2(withAdManagerAdUnitID id: String,
+              request: GAMRequest?,
+              completionHandler: @escaping GAMInterstitialAdLoadCompletionHandler)
+    func present(fromRootViewController viewController: UIViewController)
+}
+
+extension GAMInterstitialAd: LBCGAMInterstitialAdProtocol2 {
+    func load2(withAdManagerAdUnitID id: String, request: GAMRequest?, completionHandler: @escaping GAMInterstitialAdLoadCompletionHandler) {
+        Self.load(withAdManagerAdUnitID: id, request: request, completionHandler: completionHandler)
+    }
+}
