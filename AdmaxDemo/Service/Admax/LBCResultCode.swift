06abf80b5709d3bad281defeffa56540322590b9
Poudja CANESSANE <poudja.canessane@adevinta.com>
2022-02-28 16:44:05 +0100

    Create LBCResultCode

---
+++ AdmaxDemo/Service/Admax/LBCResultCode.swift
@@ -0,0 +1,12 @@
+//
+//  LBCResultCode.swift
+//  AdmaxDemo
+//
+//  Created by Poudja.canessane on 28/02/2022.
+//  Copyright © 2022 Admax. All rights reserved.
+//
+
+enum LBCResultCode {
+    case success
+    case failure
+}
