ced6ff61bd3b0ee08f631d7db226c9839360b73d
Poudja CANESSANE <poudja.canessane@adevinta.com>
2022-02-28 17:06:34 +0100

    Add convertToLBCResultCode in ResultCode's extension in LBCResultCode

--- AdmaxDemo/Service/Admax/LBCResultCode.swift
+++ AdmaxDemo/Service/Admax/LBCResultCode.swift
@@ -6,7 +6,18 @@
 //  Copyright © 2022 Admax. All rights reserved.
 //
 
+import AdmaxPrebidMobile
+
 enum LBCResultCode {
     case success
     case failure
+}
+
+extension ResultCode {
+    func convertToLBCResultCode() -> LBCResultCode {
+        switch self {
+        case .prebidDemandFetchSuccess: return .success
+        default: return .failure
+        }
+    }
 }
